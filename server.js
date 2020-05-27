3ca111bc7ddd410fb4cc31c1d259f9a5f7a56489
Sara Neves Pereira <sara.neves.pereira@gmail.com>
2020-05-27 09:52:40 -0700

    added packages and missing code to server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express=require("express");
-var path=require("path")
+var session=require("express-session")
+var passport=require("./config/passport")
 
 var PORT=process.env.PORT || 8080;
 
@@ -12,6 +13,11 @@ app.use(express.static("public"));
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 var router=require("./routes/api-routes.js");
 var router=require("./routes/userauth.js");
