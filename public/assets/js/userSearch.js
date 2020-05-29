21b5cb9194ebb1bfe1fd8ca4dc45a9cc0294a48b
Sara Neves Pereira <sara.neves.pereira@gmail.com>
2020-05-29 14:37:59 -0700

    removed local host uri from client side js

--- public/assets/js/userSearch.js
+++ public/assets/js/userSearch.js
@@ -1,12 +1,12 @@
 $(document).ready(()=>{
-    uri = "http://localhost:8080/"
+    // uri = "http://localhost:8080/"
 
     $("#run").on("click",(event)=>{
         
         var text=$("#movies").val().trim();
 
         $.ajax({
-            url:uri + "user/movie/" +text,
+            url: "/user/movie/" +text,
             method:"GET"
             // success: function(res){
             //     console.log(res)
