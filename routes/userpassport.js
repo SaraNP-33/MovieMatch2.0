7ad056f9aa63e230d48674f36f8f7a84e2309f47
Sara Neves Pereira <sara.neves.pereira@gmail.com>
2020-05-29 16:31:22 -0700

    changed login redirect to welcome page

--- routes/userpassport.js
+++ routes/userpassport.js
@@ -13,7 +13,7 @@ var passport = require("../config/passport");
     // Since we're doing a POST with javascript, we can't actually redirect that post into a GET request
     // So we're sending the user back the route to the members page because the redirect will happen on the front end
     // They won't get this or even be able to access this page if they aren't authed
-    res.json("/user/movie");
+    res.json("/welcomepage");
   });
 //
   // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
